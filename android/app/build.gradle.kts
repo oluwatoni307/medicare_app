d1e4bc9bed6bdd006419f3e6dfb7f4b935de1626
Oluwatoni Emmanuel <emmanueltoni307@gmail.com>
2025-07-29 09:50:37 +0100

    jj

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    id "com.android.application"
+    id "kotlin-android"
+    id "dev.flutter.flutter-gradle-plugin"
+}
+
 android {
     namespace = "com.example.medicare_app"
     compileSdk = flutter.compileSdkVersion
@@ -7,6 +13,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        coreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -31,4 +38,8 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:2.0.4'
 }
